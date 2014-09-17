a64b7cfe3abc48f8ebd53a15076f34d2c807c27f
unknown <dmbo0314@WSM-0547.netcracker.com>
2014-09-17 13:23:46 +0400

    task1 implemented

--- task1-openmp-integral/omp-integral.cpp
+++ task1-openmp-integral/omp-integral.cpp
@@ -10,12 +10,14 @@ using namespace std;
 #include <cmath>
 #include <cstdlib>
 
+#include "utils.h"
+
 double f(double x) {
-    return x / sin(x);
+    return sin(x) / x;
 }
 
-const double interval_begin = 1.0;
-const double interval_end = 10.0;
+const double INTERVAL_BEGIN = 1.0;
+const double INTERVAL_END = 10.0;
 
 void printUsageAndDie();
 
@@ -28,16 +30,33 @@ int main(int argc, char *argv[])
     int numberOfThreads = atoi(argv[2]);
     int numberOfPoints = atoi(argv[4]);
 
+    double results[numberOfThreads];
+
     omp_set_num_threads(numberOfThreads);
 
-    #pragma omp parallel
+    #pragma omp parallel shared(results)
     {
-        int threadNumber = omp_get_num_threads();
         int threadId = omp_get_thread_num();
+        int myStart = computeStartPoint(numberOfPoints, numberOfThreads, threadId);
+        int myEnd = computeEndPoint(numberOfPoints, numberOfThreads, threadId);
 
-        #pragma omp critical
-        cout << threadId << endl;
+        results[threadId] = 0.0;
+
+        for (int i = myStart; i < myEnd; i++) {
+            double xStart = INTERVAL_BEGIN + (INTERVAL_END - INTERVAL_BEGIN) / numberOfPoints * i;
+            double xEnd = INTERVAL_BEGIN + (INTERVAL_END - INTERVAL_BEGIN) / numberOfPoints * (i + 1);
+            double dx = xEnd - xStart;
+
+            results[threadId] += dx * (f(xStart) - f(xEnd) / 2.0);
+        }
     }
+
+    double result = 0.0;
+    for (int i = 0; i < numberOfThreads; i++) {
+        result += results[i];
+    }
+
+    cout << result << endl;
 
     return 0;
 }
---
+++ task1-openmp-integral/utils.h
@@ -0,0 +1,8 @@
+#ifndef TASK1_UTILS_H_
+#define TASK1_UTILS_H_
+
+int computeEndPoint(int points, int numThreads, int id);
+
+int computeStartPoint(int points, int numThreads, int id);
+
+#endif
