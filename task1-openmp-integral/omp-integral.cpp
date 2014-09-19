4b0df222e22e419c63061d3e4470dbf22ece11b6
dmitru <dmitry@ASPCartmansMacbookAir-2.local>
2014-09-19 20:51:23 +0400

    Fixed task 1

--- task1-openmp-integral/omp-integral.cpp
+++ task1-openmp-integral/omp-integral.cpp
@@ -10,14 +10,9 @@ using namespace std;
 #include <cmath>
 #include <cstdlib>
 
-#include "utils.h"
-
 double f(double x) {
     return sin(x) / x;
 }
-
-const double INTERVAL_BEGIN = 1.0;
-const double INTERVAL_END = 10.0;
 
 void printUsageAndDie();
 
@@ -27,33 +22,29 @@ int main(int argc, char *argv[])
         printUsageAndDie();
     }
 
-    int numberOfThreads = atoi(argv[2]);
-    int numberOfPoints = atoi(argv[4]);
+    int numberOfThreads = atoi(argv[1]);
+    int numberOfPoints = atoi(argv[2]);
+    double start = atof(argv[3]);
+    double end = atof(argv[4]);
 
-    double results[numberOfThreads];
+    if (start <= 0.0 || start >= end || numberOfThreads <= 0 
+        || numberOfPoints <= 0) {
+        cerr << "Invalid arguments" << endl;
+        exit(EXIT_FAILURE);
+    }
 
     omp_set_num_threads(numberOfThreads);
 
-    #pragma omp parallel shared(results)
-    {
-        int threadId = omp_get_thread_num();
-        int myStart = computeStartPoint(numberOfPoints, numberOfThreads, threadId);
-        int myEnd = computeEndPoint(numberOfPoints, numberOfThreads, threadId);
-
-        results[threadId] = 0.0;
-
-        for (int i = myStart; i < myEnd; i++) {
-            double xStart = INTERVAL_BEGIN + (INTERVAL_END - INTERVAL_BEGIN) / numberOfPoints * i;
-            double xEnd = INTERVAL_BEGIN + (INTERVAL_END - INTERVAL_BEGIN) / numberOfPoints * (i + 1);
-            double dx = xEnd - xStart;
-
-            results[threadId] += dx * (f(xStart) - f(xEnd) / 2.0);
-        }
-    }
-
     double result = 0.0;
-    for (int i = 0; i < numberOfThreads; i++) {
-        result += results[i];
+    #pragma omp parallel 
+    {
+        #pragma omp for reduction(+:result)
+        for (int i = 0; i < numberOfPoints; i++) {
+            double xStart = start + (end - start) / numberOfPoints * i;
+            double xEnd = start + (end - start) / numberOfPoints * (i + 1);
+            double dx = xEnd - xStart;
+            result += dx * (f(xStart) - f(xEnd) / 2.0);
+        }
     }
 
     cout << result << endl;
@@ -62,7 +53,7 @@ int main(int argc, char *argv[])
 }
 
 void printUsageAndDie() {
-    cerr << "Usage: ./program -p <number of threads> -n <number of points>"
+    cerr << "Usage: ./program <number of threads> <number of points> <start> <end>"
             << endl;
     exit(EXIT_FAILURE);
 }
