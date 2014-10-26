5edb3575f7f91afc81be32d2a0957722ead937b5
dmitru <dmitriy.borodiy@gmail.com>
2014-10-26 19:11:19 +0400

    ...

--- task3-parallelizing-loops/run_comparizon_1.sh
+++ task3-parallelizing-loops/run_comparizon_1.sh
@@ -12,16 +12,17 @@ PROG_PARALLEL=./parallel_loops/build/parallel_1
 SERIAL_FILENAME=./results_1_serial
 PARALLEL_FILENAME=./results_1_parallel
 
-echo "Will run programs $n times"
+echo "Will run programs $n times" 1>&2
 
 echo "" > results_1_serial
 echo "" > results_1_parallel
 
 i=0
 while [ "$i" -lt "$n" ]; do
-    echo "Run ${i}th iteration..."
-    $PROG_SERIAL   >> $SERIAL_FILENAME 
-    $PROG_PARALLEL >> $PARALLEL_FILENAME
+    echo "Run ${i}th iteration..." 1>&2
+    res_serial=`$PROG_SERIAL`
+    res_parallel=`$PROG_PARALLEL`
+    echo $res_serial $res_parallel
     i=$((i+1))
 done
 
